b380f1117b761e0e4e5af403847efdca935418c1
irishmikey <mikeygeraghty@gmail.com>
2024-02-15 19:02:48 +0000

    version made on my pc

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,9 +1,10 @@
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import NavBar from "./navBar";
+import Footer from "./footer";
 const poppins = Poppins({
   subsets: ["latin"],
-  weight: ["200", "400", "500"],
+  weight: ["200", "400", "500", "600", "700"],
   variable: "--font-poppins",
 });
 
@@ -15,11 +16,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+<<<<<<< HEAD
       <body
         className={`${poppins.className} lg:max-w-[1440px] flex h-screen flex-col`}
       >
+=======
+      <body className={`${poppins.className} lg:m-auto `}>
+>>>>>>> 9482804e0be3fb3ca88b937879795109b02bcdab
         <NavBar />
         {children}
+        <Footer />
       </body>
     </html>
   );
--- app/navBar.jsx
+++ app/navBar.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 "use client";
 
 import Link from "next/link";
@@ -125,6 +126,44 @@ export default function NavBar() {
           </li>
         </ul>
       </div>
+=======
+export default function NavBar() {
+  return (
+    <nav className="my-2 flex w-full px-2 shadow-md lg:mt-5 lg:items-center lg:justify-center">
+      <div className="lg:w-[1440px]">
+        <span className="lg:text-xl">Casa Magic</span>
+      </div>
+      {/* <div>
+        <ul className="lg:px-[20px] py-1 ">
+          <li className="flex gap-8">
+            <a
+              className="lg:text-[20px] transition-all hover:font-semibold"
+              href="#home"
+            >
+              Home
+            </a>
+            <a
+              className="lg:text-[20px] text-[17px] transition-all hover:font-semibold"
+              href="#about"
+            >
+              About
+            </a>
+            <a
+              className="lg:text-[20px] text-[17px] transition-all hover:font-semibold"
+              href="#gallery"
+            >
+              Contact
+            </a>
+            <a
+              className="lg:text-[20px] text-[17px] transition-all hover:font-semibold"
+              href="#contact"
+            >
+              Contact
+            </a>
+          </li>
+        </ul>
+      </div> */}
+>>>>>>> 9482804e0be3fb3ca88b937879795109b02bcdab
     </nav>
   );
 }
